42a0a569d79c8345422ccf12cc5aae0d3ef68cf2
John <devjn@jn-arts.com>
2020-08-23 10:55:49 +0300

    Create installv2jnlib.gradle.kts

---
+++ installv2jnlib.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    signing
+}
+
+group = "com.example"
+version = "1.0"
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            // Creates a Maven publication called "release".
+            release(MavenPublication) {
+                // Applies the component for the release build variant.
+                from components.release
+
+                // You can then customize attributes of the publication as shown below.
+                groupId = 'com.example.MyLibrary'
+                artifactId = 'final'
+                version = '1.0'
+            }
+            
+            pom {
+                name.set("My Library")
+                description.set("A concise description of my library")
+                url.set("http://www.example.com/library")
+                properties.set(mapOf(
+                    "myProp" to "value",
+                    "prop.with.dots" to "anotherValue"
+                ))
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("johnd")
+                        name.set("John Doe")
+                        email.set("john.doe@example.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://example.com/my-library.git")
+                    developerConnection.set("scm:git:ssh://example.com/my-library.git")
+                    url.set("http://example.com/my-library/")
+                }
+            }
+            
+        }
+    }
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
