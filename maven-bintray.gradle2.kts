27efaa0cc4e46f8c174c0306d3e82064c5737777
John <devjn@jn-arts.com>
2020-08-30 15:45:28 +0300

    Create maven-bintray.gradle2.kts

---
+++ maven-bintray.gradle2.kts
@@ -0,0 +1,47 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                name.set(artifactName)
+                description.set(artifactDescription)
+                url.set(artifactUrl)
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("devjn")
+                        name.set("John")
+                        email.set("devjn@jn-arts.com")
+                    }
+                }
+                scm {
+                    connection.set(gitUrl)
+                    developerConnection.set(gitUrl)
+                    url.set(siteUrl)
+                }
+            }
+        }
+    }
+    
+    repositories {
+        maven {
+            name = "bintray"
+            credentials {
+                username = Secrets.Bintray.user
+                password = Secrets.Bintray.apiKey
+            }
+            url = uri(bintrayUri)
+        }
+    }
+    
+}
